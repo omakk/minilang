ec1b624b58e9522cfca878f8d380599ad6848916
Omar Akkila <omakkila@gmail.com>
2017-02-04 03:01:38 -0500

    Add pretty_printer execution to main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "ast.h"
+#include "pretty.h"
 
 int yyparse();
 ASTNode  *ast;
@@ -9,5 +10,8 @@ int main()
     if (yyparse() == 0)
             printf("VALID\n");
     
+    FILE *f = fopen("ast.min", "w");
+    pretty_print(f, ast);
+
     return 0;
 }
