1d75dedc71cda1497c49faa31f63e662183fe0d4
Omar Akkila <omakkila@gmail.com>
2017-02-09 00:14:50 -0500

    Very minor change

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "ast.h"
@@ -9,12 +10,16 @@ ASTNode  *ast;
 
 char *pretty_output_file (char *s)
 {
-        char *fname;
+        char *fname, *res;
         const char slash = '/';
 
         fname = strrchr(s, slash);
-        char *res = (char *) malloc(sizeof(fname)+8);
-        sscanf(fname, "/%[^.].min", res);
+        res = (char *) malloc(sizeof(fname)+8);
+        if (fname != NULL)
+                sscanf(fname, "/%[^.].min", res);
+        else
+                sscanf(s, "%[^.].%*s", res);
+        
         res = strcat(res, ".pretty.min");
         
         return res;
@@ -22,15 +27,19 @@ char *pretty_output_file (char *s)
 
 int main(int argc, char *argv[])
 {
+    char *out_fn;
+    FILE *f;
+
     yyin = fopen(argv[1], "r");
     if (yyparse() == 0)
             printf("VALID\n");
     
-    //printf("%s\n", pretty_output_file(argv[1]));
-    char *out_fn = pretty_output_file(argv[1]);
-    FILE *f = fopen(out_fn, "w");
+    out_fn = pretty_output_file(argv[1]);
+    f = fopen(out_fn, "w");
     printf("Printing ast to %s...\n", out_fn);
     pretty_print(f, ast, 0);
+    
+    free(out_fn);
     fclose(f);
 
     return 0;
