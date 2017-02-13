65f43237e0fe2f129fb4c91570b91c03a646f6d5
Omar Akkila <omakkila@gmail.com>
2017-02-12 19:52:25 -0500

    Construct symbol table and print table to file

--- src/main.c
+++ src/main.c
@@ -2,45 +2,63 @@
 #include <stdio.h>
 #include <string.h>
 #include "ast.h"
+#include "debug.h"
 #include "pretty.h"
+#include "symbol.h"
 
 int yyparse();
 extern FILE *yyin;
 ASTNode  *ast;
 
-char *pretty_output_file (char *s)
+char *change_file_extension (const char *s, const char *extension)
 {
-        char *fname, *res;
+        char *dup, *fname, *res;
         const char slash = '/';
 
-        fname = strrchr(s, slash);
-        res = (char *) malloc(sizeof(fname)+8);
-        if (fname != NULL)
+        dup = strdup(s);
+        fname = strrchr(dup, slash);
+        if (fname != NULL) {
+                res = malloc(strlen(fname)+ strlen(extension) - 3); /* (3 = strlen(".min") - null tbyte) */
                 sscanf(fname, "/%[^.].min", res);
-        else
-                sscanf(s, "%[^.].%*s", res);
+        } else {
+                res = malloc(strlen(dup)+ strlen(extension) - 3); /* (3 = strlen(".min") - null tbyte) */
+                sscanf(dup, "%[^.].min", res);
+        }
         
-        res = strcat(res, ".pretty.min");
-        
+        strcat(res, extension);
+       
+        free(dup);
+
         return res;
 }   
 
-int main(int argc, char *argv[])
+int main(int argc, const char *argv[])
 {
-    char *out_fn;
-    FILE *f;
+    char *pretty_out_fn;
+    char *sym_out_fn;
+    FILE *pretty_f;
+    FILE *sym_f;
 
     yyin = fopen(argv[1], "r");
-    if (yyparse() == 0)
-            printf("VALID\n");
     
-    out_fn = pretty_output_file(argv[1]);
-    f = fopen(out_fn, "w");
-    printf("Printing ast to %s...\n", out_fn);
-    pretty_print(f, ast, 0);
+    yyparse();
     
-    free(out_fn);
-    fclose(f);
+    pretty_out_fn = change_file_extension(argv[1], ".pretty.min");
+    sym_out_fn    = change_file_extension(argv[1], ".symbol.txt");
+    
+    pretty_f = fopen(pretty_out_fn, "w");
+    DBG(("Printing ast to %s...\n", pretty_out_fn));
+    pretty_print(pretty_f, ast, 0);
+    free(pretty_out_fn);
+    fclose(pretty_f);
+    
+    sym_f = fopen(sym_out_fn, "w");
+    DBG(("Printing symbol table to %s...\n", sym_out_fn));
+    make_sym_table(sym_f, ast);
+    free(sym_out_fn);
+    fclose(sym_f);
+
+    printf("VALID\n");
 
     return 0;
 }
